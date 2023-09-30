960a87f774742d50748c2120504cba1b84d500f8
plassa_r <plassa_r@etna-alternance.net>
2023-09-30 22:03:15 +0200

    ✨ Add IV calculator

--- src/routes/iv-calculator/+page.svelte
+++ src/routes/iv-calculator/+page.svelte
@@ -1,3 +1,42 @@
-<div>
-    <h1>Bienvenue sur le calculateur d'IV pokémon !</h1>
+<script>
+    let attack = 0;
+    let defense = 0;
+    let hp = 0;
+    let iv = 0;
+
+    function calculIV() {
+        iv =  Math.floor((attack + defense + hp) / 45 * 100);
+    }
+</script>
+
+<div class="h-[95vh]">
+    <h1 class="text-2xl text-center py-4">Bienvenue sur le calculateur d'IV pokémon !</h1>
+    <h2 class="text-center text-xl py-3 pb-12">Vous pouvez ici calculer les IV de votre pokémon en fonction de ses statistiques.</h2>
+    
+    <div class="w-1/2 mx-auto flex gap-10 flex-col justify-center">
+        <div>
+            <input type="range" id="volume" name="volume" min="0" max="15" bind:value={attack} />
+            <label for="volume">Attaque : {attack}</label>
+        </div>
+        <div>
+            <input type="range" id="volume" name="volume" min="0" max="15" bind:value={defense} />
+            <label for="volume">Défense : {defense}</label>
+        </div>
+        <div>
+            <input type="range" id="volume" name="volume" min="0" max="15" bind:value={hp} />
+            <label for="volume">PV : {hp}</label>
+        </div>
+    </div>
+    <button
+    on:click={ () => {
+        calculIV();
+        }
+    }
+    class="mx-auto flex justify-center mb-6 rounded-xl bg-cyan-500 text-white w-fit py-1 px-6 text-lg">
+    Calculer
+    </button>
+    <p class="text-center text-xl">
+            IV : {iv}
+    </p>
+
 </div>
